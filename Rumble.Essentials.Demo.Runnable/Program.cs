deede17a1567c32fbfdebb3554f7f296c2d76aa6
Roman Malyi <36127049+rumrunner0@users.noreply.github.com>
2023-06-24 20:38:22 +0300

    Added demo application for testing purposes

---
+++ Rumble.Essentials.Demo.Runnable/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using Rumble.Essentials;
+using Serilog;
+
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+
+var settings = Essential.OfType<Settings>();
+var logger = Essential.OfType<ILogger>();
+Log.Logger = logger;
+
+logger.ForContext<Program>();
+logger.Information("Application has been started");
+
+// ...
+
+logger.Information("Application has been shut down");
+logger.Information("");
+
+Log.CloseAndFlush();
+Environment.Exit(EnvironmentExitCode.Success);
